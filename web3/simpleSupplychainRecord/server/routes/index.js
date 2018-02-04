8eb5f34d4d2376923e14ee9dbc72993858d6ee12
NIC619 <twedusuck@gmail.com>
2018-02-04 15:02:12 +0800

    Drug detail and add Drug

--- web3/simpleSupplychainRecord/server/routes/index.js
+++ web3/simpleSupplychainRecord/server/routes/index.js
@@ -58,295 +58,96 @@ router.get('/', function(req, res) {
 	res.render('index', {title: _title, isAuthorized: true, drugManufacturerList: drugManufacturers, debugMsg: _debugMsg});
 });
 
-/* GET new patient data */
+/* GET drug detail */
 router.get('/getDrugByName', function(req, res) {
 	var _debugMsg = "";
+	var drugDetail;
+	var _upstreamDrugList = [];
+	var _downstreamDrugList = [];
 	if(contractDrugSupplyChainRecord == undefined) {
 		_debugMsg = "Contract not yet deployed!";
-		res.render('index', {title: _title, isAuthorized: true, drugManufacturerList: drugManufacturers, debugMsg: _debugMsg});
+		res.render('index', {title: _title, isAuthorized: true, drugManufacturerList: drugManufacturers, drug: drugDetail, upstreamDrugList: _upstreamDrugList, downstreamDrugList: _downstreamDrugList, debugMsg: _debugMsg});
 	}
 	else {
 		console.log("Drug detail inquery received, drug name: " + req.query.name);
-		var drugDetail = {"name": req.query.name};
+		drugDetail = {"name": req.query.name};
 		funcDrugSupplyChainRecord.getDrugOwner(contractDrugSupplyChainRecord, authority, req.query.name).then(function(owner){
 			drugDetail["owner"] = owner;
 			if(owner == "0x0000000000000000000000000000000000000000") {
 				_debugMsg = "Drug '" + req.query.name + "' not found.";
 				console.log(_debugMsg);
-				res.render('drug', {title: _title, isAuthorized: true, drugManufacturerList: drugManufacturers, drug: drugDetail, debugMsg: _debugMsg});
+				res.render('drug', {title: _title, isAuthorized: true, drugManufacturerList: drugManufacturers, drug: drugDetail, upstreamDrugList: _upstreamDrugList, downstreamDrugList: _downstreamDrugList, debugMsg: _debugMsg});
 				return Promise.reject();
 			}
-			// else {
-			// 	return funcDrugSupplyChainRecord.getDrugAmount(;
-			// }
-		}).then(function(){
-			res.render('drug', {title: _title, isAuthorized: true, drugManufacturerList: drugManufacturers, drug: drugDetail, debugMsg: _debugMsg});
+			else {
+				return funcDrugSupplyChainRecord.getDrugDetail(contractDrugSupplyChainRecord, authority, req.query.name);
+			}
+		}).then(function(_drugDetail){
+			drugDetail["amount"] = _drugDetail[1].toString();
+			drugDetail["manuDate"] = _drugDetail[2];
+			drugDetail["expDate"] = _drugDetail[3];
+			drugDetail["upstreamDrugAmount"] = _drugDetail[4].toString();
+			drugDetail["downstreamDrugAmount"] = _drugDetail[5].toString();
+			console.log("Drug detail inquery processed, drug detail: " + drugDetail);
+ 			res.render('drug', {title: _title, isAuthorized: true, drugManufacturerList: drugManufacturers, drug: drugDetail, upstreamDrugList: _upstreamDrugList, downstreamDrugList: _downstreamDrugList, debugMsg: _debugMsg});
 		}).catch(function(exception) {
 			console.log("Get drug detail terminated.");
 		});
 	}
 });
 
-/* POST new patient data */
-router.post('/newRecord', function(req, res) {
-	console.log("new Record for " + req.body.addr);
-	contractDrugSupplyChainRecord.newPatientData(req.body.addr, req.body.name, req.body.sex, req.body.age, req.body.bdate,
-										req.body.mednumber, req.body.id, req.body.record, req.body.status, req.body.content,
-										{from: authority, gas: 1000000}, function(err, txid){
-		if(err) {
-			console.log("Error in newPatientData");
-			res.render('index', {title: _title, patientDataList: [], debugMsg: "Error adding record!"});
-		}
-		else {
-			var patientData = {};
-			patientData["addr"] = req.body.addr;
-			patientData["name"] = req.body.name;
-			patientData["sex"] = req.body.sex;
-			patientData["age"] = req.body.age;
-			patientData["bdate"] = req.body.bdate;
-			patientData["mednumber"] = req.body.mednumber;
-			patientData["id"] = req.body.id;
-			patientData["record"] = req.body.record;
-			patientData["status"] = req.body.status;
-			patientData["content"] = req.body.content;
-			console.log("add new patient data success: " + JSON.stringify(patientData, null, 4));
-	
-			res.render('index', {title: _title, patientDataList: [patientData], debugMsg: ""});
-		}
-	});
-	
-});
-
-/* GET modify patient data*/
-// router.get('/modify', function(req, res) {
-// 	var _debugMsg = ""
-// 	if(contractDrugSupplyChainRecord === undefined) {
-// 		_debugMsg = "Contract not yet deployed!";
-// 		res.render('index', {title: _title, patientDataList: [], debugMsg: _debugMsg});
-// 		return;
-// 	}
-// 	var patientData = {};
-// 	patientData["addr"] = req.query.addr;
-// 	patientData["name"] = contractDrugSupplyChainRecord.getName(req.query.addr);
-// 	patientData["sex"] = contractDrugSupplyChainRecord.getSex(req.query.addr);
-// 	patientData["age"] = contractDrugSupplyChainRecord.getAge(req.query.addr).toString();
-// 	patientData["bdate"] = contractDrugSupplyChainRecord.getBirthdate(req.query.addr);
-// 	patientData["mednumber"] = contractDrugSupplyChainRecord.getMednumber(req.query.addr);
-// 	patientData["id"] = contractDrugSupplyChainRecord.getId(req.query.addr);
-// 	patientData["record"] = contractDrugSupplyChainRecord.getRecord(req.query.addr).toString();
-// 	patientData["status"] = contractDrugSupplyChainRecord.getStatus(req.query.addr);
-// 	contractDrugSupplyChainRecord.getContent(req.query.addr, function(err, content){
-// 		if(err) {
-// 			_debugMsg = "Error in reading patient data";
-// 			console.log(_debugMsg);
-// 			res.render('modify', {title: _title, patientDataList: [], debugMsg: _debugMsg});
-// 		}
-// 		else {
-// 			if(content) {
-// 				patientData["content"] = content
-// 				console.log("complete reading patient data: " + JSON.stringify(patientData, null, 4));
-// 				res.render('modify', {title: _title, patientDataList: [patientData], debugMsg: ""});
-// 			}
-// 		}
-// 	});
-// });
-
-/* POST modify patient data*/
-router.post('/modify', function(req, res) {
-	console.log("modify Record for " + req.body.addr);
+/* GET new drug */
+router.get('/addNewDrug', function(req, res) {
 	var _debugMsg = "";
-	var newPatientData = {};
-	newPatientData["addr"] = req.body.addr;
-	newPatientData["name"] = req.body.name;
-	newPatientData["sex"] = req.body.sex;
-	newPatientData["age"] = req.body.age;
-	newPatientData["bdate"] = req.body.bdate;
-	newPatientData["mednumber"] = req.body.mednumber;
-	newPatientData["id"] = req.body.id;
-	newPatientData["record"] = req.body.record;
-	newPatientData["status"] = req.body.status;
-	newPatientData["content"] = req.body.content;
 
-	var oldPatientData = {};
-	oldPatientData["addr"] = req.body.addr;
-	oldPatientData["name"] = req.body.oldname;
-	oldPatientData["sex"] = req.body.oldsex;
-	oldPatientData["age"] = req.body.oldage;
-	oldPatientData["bdate"] = req.body.oldbdate;
-	oldPatientData["mednumber"] = req.body.oldmednumber;
-	oldPatientData["id"] = req.body.oldid;
-	oldPatientData["record"] = req.body.oldrecord;
-	oldPatientData["status"] = req.body.oldstatus;
-	oldPatientData["content"] = req.body.oldcontent;
-	var patientData = {};
-
-	Promise.resolve().then(function(){
-		if(oldPatientData.name != newPatientData.name) {
-			return funcDrugSupplyChainRecord.modifyName(contractDrugSupplyChainRecord, authority, req.body.addr, newPatientData.name);
-		}
-		else {
-			return Promise.resolve();
-		}
-	}).then(function(){
-		oldPatientData.name = newPatientData.name;
-		if(oldPatientData.sex != newPatientData.sex) {
-			return funcDrugSupplyChainRecord.modifySex(contractDrugSupplyChainRecord, authority, req.body.addr, newPatientData.sex);
-		}
-		else {
-			return Promise.resolve();
-		}
-	}).then(function(){
-		oldPatientData.sex = newPatientData.sex;
-		if(oldPatientData.age != newPatientData.age) {
-			return funcDrugSupplyChainRecord.modifyAge(contractDrugSupplyChainRecord, authority, req.body.addr, newPatientData.age);
-		}
-		else {
-			return Promise.resolve();
-		}
-	}).then(function(){
-		oldPatientData.age = newPatientData.age;
-		if(oldPatientData.bdate != newPatientData.bdate) {
-			return funcDrugSupplyChainRecord.modifyBirthdate(contractDrugSupplyChainRecord, authority, req.body.addr, newPatientData.bdate);
-		}
-		else {
-			return Promise.resolve();
-		}
-	}).then(function(){
-		oldPatientData.bdate = newPatientData.bdate;
-		if(oldPatientData.mednumber != newPatientData.mednumber) {
-			return funcDrugSupplyChainRecord.modifyMednumber(contractDrugSupplyChainRecord, authority, req.body.addr, newPatientData.mednumber);
-		}
-		else {
-			return Promise.resolve();
-		}
-	}).then(function(){
-		oldPatientData.mednumber = newPatientData.mednumber;
-		if(oldPatientData.id != newPatientData.id) {
-			return funcDrugSupplyChainRecord.modifyId(contractDrugSupplyChainRecord, authority, req.body.addr, newPatientData.id);
-		}
-		else {
-			return Promise.resolve();
-		}
-	}).then(function(){
-		oldPatientData.id = newPatientData.id;
-		if(oldPatientData.record != newPatientData.record) {
-			return funcDrugSupplyChainRecord.modifyRecord(contractDrugSupplyChainRecord, authority, req.body.addr, newPatientData.record);
-		}
-		else {
-			return Promise.resolve();
-		}
-	}).then(function(){
-		oldPatientData.record = newPatientData.record;
-		if(oldPatientData.status != newPatientData.status) {
-			return funcDrugSupplyChainRecord.modifyStatus(contractDrugSupplyChainRecord, authority, req.body.addr, newPatientData.status);
-		}
-		else {
-			return Promise.resolve();
-		}
-	}).then(function(){
-		oldPatientData.status = newPatientData.status;
-		if(oldPatientData.content != newPatientData.content) {
-			return funcDrugSupplyChainRecord.modifyContent(contractDrugSupplyChainRecord, authority, req.body.addr, newPatientData.content);
-		}
-		else {
-			return Promise.resolve();
-		}
-	}).then(function(){
-		oldPatientData.content = newPatientData.content;
-		res.render('modify', {title: _title, patientDataList: [oldPatientData], debugMsg: _debugMsg});
-	}).catch(function(exception){
-		console.log(exception);
-		_debugMsg = "Error modifying patient data";
-		res.render('modify', {title: _title, patientDataList: [oldPatientData], debugMsg: _debugMsg});
-	})
+	if(contractDrugSupplyChainRecord == undefined) {
+		_debugMsg = "Contract not yet deployed!";
+		res.render('index', {title: _title, isAuthorized: true, drugManufacturerList: drugManufacturers, debugMsg: _debugMsg});
+	}
+	else {
+		res.render('newDrug', {title: _title, isAuthorized: true, drugManufacturerList: drugManufacturers, debugMsg: _debugMsg});
+	}
 });
 
-/* GET patient data by address */
-router.get('/getPatientDataByAddr', function(req, res) {
-	var _debugMsg = ""
-	if(contractDrugSupplyChainRecord === undefined) {
+/* POST new drug */
+router.post('/addNewDrug', function(req, res) {
+	var _debugMsg = "";
+	if(contractDrugSupplyChainRecord == undefined) {
 		_debugMsg = "Contract not yet deployed!";
-		res.render('index', {title: _title, patientDataList: [], debugMsg: _debugMsg});
-		return;
+		res.render('index', {title: _title, isAuthorized: true, drugManufacturerList: drugManufacturers, debugMsg: _debugMsg});
 	}
-	var patientData = {};
-	patientData["addr"] = req.query.addr;
-	funcDrugSupplyChainRecord.getAge(contractDrugSupplyChainRecord, authority, req.query.addr).then(function(age){
-		patientData["age"] = age.toString();
-		return funcDrugSupplyChainRecord.getSex(contractDrugSupplyChainRecord, authority, req.query.addr);
-	}).then(function(sex){
-		patientData["sex"] = sex;
-		return funcDrugSupplyChainRecord.getName(contractDrugSupplyChainRecord, authority, req.query.addr);
-	}).then(function(name){
-		patientData["name"] = name;
-		return funcDrugSupplyChainRecord.getBirthdate(contractDrugSupplyChainRecord, authority, req.query.addr);
-	}).then(function(bdate){
-		patientData["bdate"] = bdate;
-		return funcDrugSupplyChainRecord.getMednumber(contractDrugSupplyChainRecord, authority, req.query.addr);
-	}).then(function(mednumber){
-		patientData["mednumber"] = mednumber;
-		return funcDrugSupplyChainRecord.getId(contractDrugSupplyChainRecord, authority, req.query.addr);
-	}).then(function(id){
-		patientData["id"] = id;
-		return funcDrugSupplyChainRecord.getRecord(contractDrugSupplyChainRecord, authority, req.query.addr);
-	}).then(function(record){
-		patientData["record"] = record.toString();
-		return funcDrugSupplyChainRecord.getStatus(contractDrugSupplyChainRecord, authority, req.query.addr);
-	}).then(function(status){
-		patientData["status"] = status;
-		return funcDrugSupplyChainRecord.getContent(contractDrugSupplyChainRecord, authority, req.query.addr);
-	}).then(function(content){
-		patientData["content"] = content;
-		res.render('modify', {title: _title, patientDataList: [patientData], debugMsg: _debugMsg});
-	}).catch(function(exception){
-		console.log(exception);
-		_debugMsg = "Error fetching patient data or data not exist";
-		res.render('index', {title: _title, patientDataList: [], debugMsg: _debugMsg});
-	})
-})
-
-/* GET patient data by attributes*/
-// router.get('/query', function(req, res) {
-// 	if(req.query.type == "name") {
-// 		contractDrugSupplyChainRecord.getName(req.query.addr, function(err, name){
-// 			if(err) {
-// 				res.send("ERROR");
-// 			}
-// 			else {
-// 				if(name) {
-// 					res.send(name)
-// 				}
-// 			}
-// 		});
-// 	}
-// 	else if(req.query.sex == "sex") {
-// 		contractDrugSupplyChainRecord.getSex(req.query.addr, function(err, sex){
-// 			if(err) {
-// 				res.send("ERROR");
-// 			}
-// 			else {
-// 				if(sex) {
-// 					res.send(sex)
-// 				}
-// 			}
-// 		});
-// 	}
-// 	else if(req.query.age == "age") {
-// 		contractDrugSupplyChainRecord.getAge(req.query.addr, function(err, age){
-// 			if(err) {
-// 				res.send("ERROR");
-// 			}
-// 			else {
-// 				if(age) {
-// 					res.send(age)
-// 				}
-// 			}
-// 		});
-// 	}
-// });
-
-
-
-
+	else {
+		console.log("Receive a request to add new drug: " + req.body.name);
+		funcDrugSupplyChainRecord.getDrugOwner(contractDrugSupplyChainRecord, authority, req.query.name).then(function(owner){
+			if(owner != "0x0000000000000000000000000000000000000000") {
+				_debugMsg = "Drug has already been registered!";
+				res.render('newDrug', {title: _title, isAuthorized: true, drugManufacturerList: drugManufacturers, debugMsg: _debugMsg});
+			}
+			else {
+				return funcDrugSupplyChainRecord.addNewDrug(contractDrugSupplyChainRecord, req.body.owner, req.body.name, req.body.manuDate, req.body.expDate, req.body.amount);
+			}
+		}).catch(function(exception){
+			_debugMsg = "Failed to add new drug";
+			res.render('newDrug', {title: _title, isAuthorized: true, drugManufacturerList: drugManufacturers, debugMsg: _debugMsg});
+		}).then(function() {
+			return funcDrugSupplyChainRecord.getDrugDetail(contractDrugSupplyChainRecord, authority, req.body.name);
+		}).then(function(_drugDetail){
+			// var drugDetail = {};
+			// drugDetail["name"] = req.body.name;
+			// drugDetail["owner"] = _drugDetail[0]
+			// drugDetail["amount"] = _drugDetail[1].toString();
+			// drugDetail["manuDate"] = _drugDetail[2];
+			// drugDetail["expDate"] = _drugDetail[3];
+			// drugDetail["upstreamDrugAmount"] = _drugDetail[4].toString();
+			// drugDetail["downstreamDrugAmount"] = _drugDetail[5].toString();
+			console.log("Request to add new drug processed, drug name: " + req.body.name);
+			res.redirect("/getDrugByName?name=" + req.body.name);
+ 			// res.render('drug', {title: _title, isAuthorized: true, drugManufacturerList: drugManufacturers, drug: drugDetail, debugMsg: _debugMsg});
+		}).catch(function(exception){
+			_debugMsg = "Failed to add new drug";
+			res.render('newDrug', {title: _title, isAuthorized: true, drugManufacturerList: drugManufacturers, debugMsg: _debugMsg});
+		});
+	}
+});
 
 module.exports = router;
