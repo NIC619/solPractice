bf855b7bc147ab45bb3adae6a8bea46b8a60328c
NIC619 <twedusuck@gmail.com>
2020-05-15 23:24:28 +0800

    Move utils to utils module

--- DecentralizedOAS/test/garbled_circuit.js
+++ DecentralizedOAS/test/garbled_circuit.js
@@ -1,93 +1,20 @@
 const GarbledCircuit = artifacts.require("GarbledCircuit");
 
-function gen_key(num_bits) {
-	var key = new Uint8Array(num_bits);
-	for (var i = 0; i < num_bits; i++) {
-		key[i] = Math.floor((Math.random() * 256));
-	}
-	return key;
-}
+const utils = require('./utils');
+gen_key = utils.gen_key;
+xor = utils.xor;
+get_entry_index = utils.get_entry_index;
+get_OR_entry_result = utils.get_OR_entry_result;
+garbling_OR_entries = utils.garbling_OR_entries;
+get_AND_entry_result = utils.get_AND_entry_result;
+garbling_AND_entries = utils.garbling_AND_entries;
+get_XOR_entry_result = utils.get_XOR_entry_result;
+garbling_XOR_entries = utils.garbling_XOR_entries;
+get_NAND_entry_result = utils.get_NAND_entry_result;
+garbling_NAND_entries = utils.garbling_NAND_entries;
+gen_update_label_table = utils.gen_update_label_table;
+decrypt_update_lable_entries = utils.decrypt_update_lable_entries;
 
-function xor(a, b) {
-	if(a.length != b.length) throw 'XORing array with different length';
-	var c = new Uint8Array(a.length);
-	for (var i = 0; i < a.length; i++) {
-		c[i] = a[i] ^ b[i];
-	}
-	return c;
-}
-
-// 0: x_0, y_0
-// 1: x_0, y_1
-// 2: x_1, y_0
-// 3: x_1, y_1
-function get_entry_index(x, y) {
-	if(x == 0 && y == 0) return 0;
-	else if(x == 0 && y == 1) return 1;
-	else if(x == 1 && y == 0) return 2;
-	else if(x == 1 && y == 1) return 3;
-	else throw 'Invalid x,y values';
-}
-
-function get_OR_entry_result(entry) {
-	if(entry == 0) return 0;
-	else return 1;
-}
-
-function garbling_OR_entries(x_0, x_1, y_0, y_1, z_0, z_1) {
-	return [xor(xor(x_0, y_0), z_0), xor(xor(x_0, y_1), z_1), xor(xor(x_1, y_0), z_1), xor(xor(x_1, y_1), z_1)];
-}
-
-function get_AND_entry_result(entry) {
-	if(entry == 3) return 1;
-	else return 0;
-}
-
-function garbling_AND_entries(x_0, x_1, y_0, y_1, z_0, z_1) {
-	return [xor(xor(x_0, y_0), z_0), xor(xor(x_0, y_1), z_0), xor(xor(x_1, y_0), z_0), xor(xor(x_1, y_1), z_1)];
-}
-
-function get_XOR_entry_result(entry) {
-	if(entry == 0 || entry == 3) return 0;
-	else return 1;
-}
-
-function garbling_XOR_entries(x_0, x_1, y_0, y_1, z_0, z_1) {
-	return [xor(xor(x_0, y_0), z_0), xor(xor(x_0, y_1), z_1), xor(xor(x_1, y_0), z_1), xor(xor(x_1, y_1), z_0)];
-}
-
-function get_NAND_entry_result(entry) {
-	if(entry == 3) return 0;
-	else return 1;
-}
-
-function garbling_NAND_entries(x_0, x_1, y_0, y_1, z_0, z_1) {
-	return [xor(xor(x_0, y_0), z_1), xor(xor(x_0, y_1), z_1), xor(xor(x_1, y_0), z_1), xor(xor(x_1, y_1), z_0)];
-}
-
-function gen_update_label_table(old_0, old_1, new_0, new_1) {
-	return [
-		xor(old_0, new_0),
-		xor(old_1, new_1),
-		web3.utils.keccak256(web3.utils.bytesToHex(new_0)),
-		web3.utils.keccak256(web3.utils.bytesToHex(new_1)),
-	];
-}
-
-function decrypt_update_lable_entries(entry_0, entry_1, input, hash_digest_0, hash_digest_1) {
-	var result = xor(input, entry_0);
-	var result_hash_digest = web3.utils.keccak256(web3.utils.bytesToHex((result)));
-	if((result_hash_digest == hash_digest_0) || (result_hash_digest == hash_digest_1)) {
-		return result
-	}
-	result = xor(input, entry_1);
-	result_hash_digest = web3.utils.keccak256(web3.utils.bytesToHex((result)));
-	if((result_hash_digest == hash_digest_0) || (result_hash_digest == hash_digest_1)) {
-		return result
-	} else {
-		throw 'Decrypt update lable entries failed: hash digest of result does not match';
-	}
-}
 
 contract('GarbledCircuit', () => {
 	it('should successfully deploy and decrypt a 7 tables circuit', async () => {
@@ -105,7 +32,7 @@ contract('GarbledCircuit', () => {
 		var table_indices = [0, 1, 2, 3, 4, 5, 6];
 		const num_results = 1;
 		// Format of table relation entry: [child_table_index, parent_table_index, is_input_x_to_parent_table]
-		// and for is_input_x_to_parent_table, 1 means yes, 0 means
+		// and for is_input_x_to_parent_table, 1 means yes, 0 means no
 		// This table relation means:
 		// paretn table of table 0 is input x of table 4
 		// paretn table of table 1 is input y of table 4
@@ -116,7 +43,7 @@ contract('GarbledCircuit', () => {
 		var table_relation = [[0, 4, 1], [1, 4, 0], [2, 5, 1], [3, 5, 0], [4, 6, 1], [5, 6, 0]];
 		// entry in same_entry_tables represents two tables that share same input
 		// Format of same_entry_tables entry: [table_1, is_input_x, table_2, is_input_x]
-		// For example: [3, 1, 1, 0] means that input y of table 3 share the same input as input x of table 1
+		// For example: [3, 1, 1, 0] means that input x of table 3 share the same input as input y of table 1
 		var same_entry_tables = [];
 		var indices_of_end_tables = [6];
 		var indices_of_initial_input_tables = [0, 1, 2, 3];
@@ -347,7 +274,7 @@ contract('GarbledCircuit', () => {
 		table_type["OR"] = [9, 10, 11];
 		table_type["AND"] = [12, 13];
 		// Format of table relation entry: [child_table_index, parent_table_index, is_input_x_to_parent_table]
-		// and for is_input_x_to_parent_table, 1 means yes, 0 means
+		// and for is_input_x_to_parent_table, 1 means yes, 0 means no
 		var table_relation = [
 			[1, 9, 1],
 			[2, 10, 1],
@@ -362,7 +289,7 @@ contract('GarbledCircuit', () => {
 		];
 		// entry in same_entry_tables represents two tables that share same input
 		// Format of same_entry_tables entry: [table_1, is_input_x, table_2, is_input_x]
-		// For example: [3, 1, 1, 0] means that input y of table 3 share the same input as input x of table 1
+		// For example: [3, 1, 1, 0] means that input x of table 3 share the same input as input y of table 1
 		var same_entry_tables = [[13, 1, 12, 1]];
 		var indices_of_end_tables = [12, 13];
 		var indices_of_initial_input_tables = [1, 2, 3, 4, 5, 6, 7, 8];
---
+++ DecentralizedOAS/test/utils.js
@@ -0,0 +1,104 @@
+function gen_key(num_bits) {
+	var key = new Uint8Array(num_bits);
+	for (var i = 0; i < num_bits; i++) {
+		key[i] = Math.floor((Math.random() * 256));
+	}
+	return key;
+}
+
+function xor(a, b) {
+	if(a.length != b.length) throw 'XORing array with different length';
+	var c = new Uint8Array(a.length);
+	for (var i = 0; i < a.length; i++) {
+		c[i] = a[i] ^ b[i];
+	}
+	return c;
+}
+
+// 0: x_0, y_0
+// 1: x_0, y_1
+// 2: x_1, y_0
+// 3: x_1, y_1
+function get_entry_index(x, y) {
+	if(x == 0 && y == 0) return 0;
+	else if(x == 0 && y == 1) return 1;
+	else if(x == 1 && y == 0) return 2;
+	else if(x == 1 && y == 1) return 3;
+	else throw 'Invalid x,y values';
+}
+
+function get_OR_entry_result(entry) {
+	if(entry == 0) return 0;
+	else return 1;
+}
+
+function garbling_OR_entries(x_0, x_1, y_0, y_1, z_0, z_1) {
+	return [xor(xor(x_0, y_0), z_0), xor(xor(x_0, y_1), z_1), xor(xor(x_1, y_0), z_1), xor(xor(x_1, y_1), z_1)];
+}
+
+function get_AND_entry_result(entry) {
+	if(entry == 3) return 1;
+	else return 0;
+}
+
+function garbling_AND_entries(x_0, x_1, y_0, y_1, z_0, z_1) {
+	return [xor(xor(x_0, y_0), z_0), xor(xor(x_0, y_1), z_0), xor(xor(x_1, y_0), z_0), xor(xor(x_1, y_1), z_1)];
+}
+
+function get_XOR_entry_result(entry) {
+	if(entry == 0 || entry == 3) return 0;
+	else return 1;
+}
+
+function garbling_XOR_entries(x_0, x_1, y_0, y_1, z_0, z_1) {
+	return [xor(xor(x_0, y_0), z_0), xor(xor(x_0, y_1), z_1), xor(xor(x_1, y_0), z_1), xor(xor(x_1, y_1), z_0)];
+}
+
+function get_NAND_entry_result(entry) {
+	if(entry == 3) return 0;
+	else return 1;
+}
+
+function garbling_NAND_entries(x_0, x_1, y_0, y_1, z_0, z_1) {
+	return [xor(xor(x_0, y_0), z_1), xor(xor(x_0, y_1), z_1), xor(xor(x_1, y_0), z_1), xor(xor(x_1, y_1), z_0)];
+}
+
+function gen_update_label_table(old_0, old_1, new_0, new_1) {
+	return [
+		xor(old_0, new_0),
+		xor(old_1, new_1),
+		web3.utils.keccak256(web3.utils.bytesToHex(new_0)),
+		web3.utils.keccak256(web3.utils.bytesToHex(new_1)),
+	];
+}
+
+function decrypt_update_lable_entries(entry_0, entry_1, input, hash_digest_0, hash_digest_1) {
+	var result = xor(input, entry_0);
+	var result_hash_digest = web3.utils.keccak256(web3.utils.bytesToHex((result)));
+	if((result_hash_digest == hash_digest_0) || (result_hash_digest == hash_digest_1)) {
+		return result
+	}
+	result = xor(input, entry_1);
+	result_hash_digest = web3.utils.keccak256(web3.utils.bytesToHex((result)));
+	if((result_hash_digest == hash_digest_0) || (result_hash_digest == hash_digest_1)) {
+		return result
+	} else {
+		throw 'Decrypt update lable entries failed: hash digest of result does not match';
+	}
+}
+
+module.exports = {
+    gen_key: gen_key,
+    xor: xor,
+    get_entry_index: get_entry_index,
+    get_OR_entry_result: get_OR_entry_result,
+    garbling_OR_entries: garbling_OR_entries,
+    get_AND_entry_result: get_AND_entry_result,
+    garbling_AND_entries: garbling_AND_entries,
+    get_XOR_entry_result: get_XOR_entry_result,
+    garbling_XOR_entries: garbling_XOR_entries,
+    get_NAND_entry_result: get_NAND_entry_result,
+    garbling_NAND_entries: garbling_NAND_entries,
+    gen_update_label_table: gen_update_label_table,
+    decrypt_update_lable_entries: decrypt_update_lable_entries,
+}
